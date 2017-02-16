bc803c3d25e8c8352e4e2b7e383aa9b14c7af7d1
Max Goldman <maxg@mit.edu>
2017-02-16 14:24:53 -0500

    Allow custom instructions to override browser text selection

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 // select the URL for easy copying
 var selection = window.getSelection();
 var range = document.createRange();
-range.selectNodeContents(document.getElementById('url'));
+var elt = document.getElementById('selected') || document.getElementById('url');
+range.selectNodeContents(elt);
 selection.removeAllRanges();
 selection.addRange(range);
